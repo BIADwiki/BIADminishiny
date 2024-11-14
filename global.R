1fa7d2603b01175f5f0a6c8bdf615aacdd81d95d
Simon <simon@elisya.org>
2024-11-14 08:47:40 +0000

    renaming and moving

---
+++ global.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(leaflet)
+library(ggmap)
+library(geosphere) 
+library(shinyTree)
+library(data.tree)
+source("functions.R")
+source("functions.database.connect.R")
+library(shinybusy)
+
+add_busy_bar(color = "#FF0000",timeout=1000)
+
+# Register your Google Maps API key
+allsites=readRDS("../data/sites_table.RDS")
+
+# Register your Google Maps API key
+register_google(key = Sys.getenv("GMAP_API"))#attention ac ma clef
+
+conn <- init.conn()
+
+coords <- cbind(allsites$Longitude, allsites$Latitude)
+
+# Function to get the list of tables from the database
+get_table_list <- function(conn) {
+    alltables <- DBI::dbListTables(conn)
+    alltables[!grepl("z.*_.*",alltables)]
+}
+
+# Retrieve field names for a specified table
+get_field_list <- function(conn, table) {
+    DBI::dbListFields(conn, table)
+}
+
+tables <- get_table_list(conn)
